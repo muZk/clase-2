39e6640f92f5386ebc090866fc35b87441abe39c
Nico <ngomez@hey.com>
2024-12-03 21:10:52 -0300

    agrega landing page

--- app.py
+++ app.py
@@ -1,26 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-   # llamado a OpenAI
-   return "Hello World!"
-
-
-@app.route('/bye')
-def bye():
-   return "Goodbye!"
-
-
-@app.route('/iniciar-sesion')
-def iniciar_sesion():
-   return "Iniciar sesión"
-
-
-@app.route('/users/<username>')
-def users(username):
-   return "Perfil de usuario: " + username
-
-
+def index():
+    return render_template('landing.html')
